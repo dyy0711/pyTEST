192db560a2f598fa35065edace6b4ecd27ba95cb
dyy0711 <18940033842@163.com>
2021-07-06 16:14:11 +0800

    完成了学生信息管理系统的所有功能

--- stusystem.py
+++ stusystem.py
@@ -197,25 +197,24 @@ def sort():
         mode=input('请选择排序方式：1按英语成绩排序  2.按python成绩排序  3.按java成绩排序  0.按总成绩排序\n')
         if mode=='1':
            student_new.sort(key=lambda x:int(x['english']),reverse=bool)
-           #save(student_new)
+           show_student(student_new)
+
         elif mode=='2':
            student_new.sort(key=lambda x:int(x['python']),reverse=bool)
-           #save(student_new)
+           show_student(student_new)
         elif mode=='3':
            student_new.sort(key=lambda x:int(x['java']),reverse=bool)
-           #save(student_new)
+           show_student(student_new)
         elif mode=='0':
            student_new.sort(key=lambda x:int(x['english'])+int(x['python'])+int(x['java']),reverse=bool)
-           #save(student_new)
+           show_student(student_new)
         else:
            print('您输入的信息有误，请重新输入')
            sort()
-        show(student_new)
     else:
         print('暂未保存学生信息')
         return
 
-    
 
 def total():
     if os.path.exists(filename):
@@ -228,22 +227,28 @@ def total():
     else:
         print('暂未保存数据信息')
 
+def show_student(list):
+    if len(list)==0:
+        print('没有找到学生信息')
+        return
+    else:
+        format_title='{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^8}'
+        print(format_title.format('ID','姓名','英语成绩','python成绩','java成绩','总成绩'))
+        format_date='{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^8}'
+    for item in list:
+        #d=dict(eval(item))
+        print(format_date.format(item.get('id'),item.get('name'),item.get('english'),item.get('python'),item.get('java'),int(item.get('english'))+int(item.get('python'))+int(item.get('java'))))
+
 def show():
-    # pass
+    student_list1=[]
     if os.path.exists(filename):
         with open(filename,'r',encoding='utf-8') as rfile:
-            student_list=rfile.readlines()
-            for item in student_list:
+            students1=rfile.readlines()
+            for item in students1:
                 d=dict(eval(item))
-            if len(d)==0:
-                print('没有查询到学生信息')
-            else:
-                format_title='{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^8}'
-                print(format_title.format('ID','姓名','英语成绩','python成绩','java成绩','总成绩'))
-                format_date='{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^8}'
-            for item in student_list:
-                d=dict(eval(item))
-                print(format_date.format(d.get('id'),d.get('name'),d.get('english'),d.get('python'),d.get('java'),int(d.get('english'))+int(d.get('python'))+int(d.get('java'))))
+                student_list1.append(d)
+            if student_list1:
+                show_student(student_list1)
     else:
         print('暂未保存学生数据信息')
 if __name__=='__main__':
