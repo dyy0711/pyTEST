cd43a80147862fa12ba3d78cdfc8b52a34cbcbfb
dyy0711 <18940033842@163.com>
2021-07-31 23:46:21 +0800

    python练习11

---
+++ 21.03.25-5.py
@@ -0,0 +1,12 @@
+#字典
+#字典的创建
+'''第一种：使用{}'''
+scores={'张三':100,'李四':98,'王五':45}
+print(scores)
+print(type(scores))
+'''使用内置函数dict()'''
+student=dict(name='jack',age=20)
+print(student)
+'''空字典'''
+d={}
+print(d)
